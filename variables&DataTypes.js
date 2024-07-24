81ad04838cf827884de35c3fdb06b391ee9f95ce
Dipali <dipalikamboj0@gmail.com>
2024-07-24 11:29:47 +0530

    1st Day

---
+++ variables&DataTypes.js
@@ -0,0 +1,36 @@
+var number = 10;
+// console.log(number);
+
+let string = "Dipali";
+// console.log(string);
+
+const isAvailable = true;
+// console.log(isAvailable);   
+
+let count = 12;
+let userName = "jarvis";
+const isLogged = false;
+let user = {
+    name : "Arun",
+    age : 22,
+    salary : 100
+};
+let arrayOfMarks = [10 , 20 , 30 , 40, 50];
+// console.log(typeof count);
+// console.log(typeof userName);
+// console.log(typeof isLogged);
+// console.log(typeof user);
+// console.log(typeof arrayOfMarks);
+
+// array is itself an object
+
+let score = 30;
+// console.log(score);
+score = 10;
+// console.log(score);
+
+const balance = 3000;;
+// balance = 2000;
+// console.log(balance);
+
+// TypeError: Assignment to constant variable.
